1f652c9be174c3b8453900c896462e8a2c930ef9
Abishek R <abishek.genex@gmail.com>
2019-05-21 13:10:29 -0700

    added packed image input for rppi_hue and rppi_saturation

--- src/imgaug/rppi_hue.cpp
+++ src/imgaug/rppi_hue.cpp
@@ -132,6 +132,124 @@ RppStatus rppi_hue_3C8U_pln_cpu(Rpp8u *pSrc, RppiSize size, Rpp8u *pDst, Rpp32f 
 
 }
 
+RppStatus rppi_hue_3C8U_pkd_cpu(Rpp8u *pSrc, RppiSize size, Rpp8u *pDst, Rpp32f hueShift = 0)
+{
+    Rpp32f *pHSV = (Rpp32f *)malloc(size.channel * size.width * size.height * sizeof(Rpp32f));
+    for (int i = 0; i < (3 * size.width * size.height); i+=3)
+    {
+        float rf, gf, bf, cmax, cmin, delta;
+        rf = ((float) pSrc[i]) / 255;
+        gf = ((float) pSrc[i + 1]) / 255;
+        bf = ((float) pSrc[i + 2]) / 255;
+        cmax = std::max(std::max(rf, gf), bf);
+        cmin = std::min(std::min(rf, gf), bf);
+        delta = cmax - cmin;
+
+        if (delta == 0)
+        {
+            pHSV[i] = 0;
+        }
+        else if (cmax == rf)
+        {
+            pHSV[i] = round(60 * fmod(((gf - bf) / delta),6));
+        }
+        else if (cmax == gf)
+        {
+            pHSV[i] = round(60 * (((bf - rf) / delta) + 2));
+        }
+        else if (cmax == bf)
+        {
+            pHSV[i] = round(60 * (((rf - gf) / delta) + 4));
+        }
+        
+        while (pHSV[i] > 360)
+        {
+            pHSV[i] = pHSV[i] - 360;
+        }
+        while (pHSV[i] < 0)
+        {
+            pHSV[i] = 360 + pHSV[i];
+        }
+
+        if (cmax == 0)
+        {
+            pHSV[i + 1] = 0;
+        }
+        else
+        {
+            pHSV[i + 1] = delta / cmax;
+        }
+
+        pHSV[i + 2] = cmax;
+
+    }
+    
+    for (int i = 0; i < (3 * size.width * size.height); i+=3)
+    {
+        pHSV[i] += hueShift;
+        while (pHSV[i] > 360)
+        {
+            pHSV[i] = pHSV[i] - 360;
+        }
+        while (pHSV[i] < 0)
+        {
+            pHSV[i] = 360 + pHSV[i];
+        }
+    }
+
+    for (int i = 0; i < (3 * size.width * size.height); i+=3)
+    {
+        float c, x, m, rf, gf, bf;
+        c = pHSV[i + 2] * pHSV[i + 1];
+        x = c * (1 - abs((fmod((pHSV[i] / 60), 2)) - 1));
+        m = pHSV[i + 2] - c;
+        
+        if ((0 <= pHSV[i]) && (pHSV[i] < 60))
+        {
+            rf = c;
+            gf = x;
+            bf = 0;
+        }
+        else if ((60 <= pHSV[i]) && (pHSV[i] < 120))
+        {
+            rf = x;
+            gf = c;
+            bf = 0;
+        }
+        else if ((120 <= pHSV[i]) && (pHSV[i] < 180))
+        {
+            rf = 0;
+            gf = c;
+            bf = x;
+        }
+        else if ((180 <= pHSV[i]) && (pHSV[i] < 240))
+        {
+            rf = 0;
+            gf = x;
+            bf = c;
+        }
+        else if ((240 <= pHSV[i]) && (pHSV[i] < 300))
+        {
+            rf = x;
+            gf = 0;
+            bf = c;
+        }
+        else if ((300 <= pHSV[i]) && (pHSV[i] < 360))
+        {
+            rf = c;
+            gf = 0;
+            bf = x;
+        }
+
+        pDst[i] = (Rpp8u) round((rf + m) * 255);
+        pDst[i + 1] = (Rpp8u) round((gf + m) * 255);
+        pDst[i + 2] = (Rpp8u) round((bf + m) * 255);
+    }
+
+    return RPP_SUCCESS;
+
+}
+
 
 
 
@@ -181,9 +299,26 @@ void display(Rpp8u *pArr, RppiSize size)
     }
 }
 
+void displayPacked(Rpp8u *pArr, RppiSize size)
+{
+    int p = 0;
+    for (int i = 0; i < (size.width * size.height); i++)
+    {
+        printf("%d,%d,%d\t\t", *(pArr + p), *(pArr + p + 1), *(pArr + p + 2));
+        if (((i + 1) % size.width) == 0)
+        {
+            printf("\n");
+        }
+        p += 3;
+    }
+}
+
 int main()
 {
     RppiSize size;
+    int type;
+    printf("\nEnter 1 = planar, 2 = packed: ");
+    scanf("%d", &type);
     printf("\nNumber of channels is fixed: 3\n");
     size.channel = 3;
     printf("Enter width of image in pixels: ");
@@ -204,11 +339,20 @@ int main()
 
     cast(isrc, src, size);
 
-    printf("\nInput:\n\n");
-    display(src, size);
-
-    rppi_hue_3C8U_pln_cpu(src, size, dst, hueShift);
-
-    printf("\nOutput of Hue Modification:\n\n");
-    display(dst, size);
+    if (type == 1)
+    {
+        printf("\nInput:\n\n");
+        display(src, size);
+        rppi_hue_3C8U_pln_cpu(src, size, dst, hueShift);
+        printf("\nOutput of Hue Modification:\n\n");
+        display(dst, size);
+    }
+    else if (type == 2)
+    {
+        printf("\nInput:\n\n");
+        displayPacked(src, size);
+        rppi_hue_3C8U_pkd_cpu(src, size, dst, hueShift);
+        printf("\nOutput of Hue Modification:\n\n");
+        displayPacked(dst, size);
+    }
 }
