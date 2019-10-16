080a02a75ed66dd56cef5ee582ca379e1c831d48
r-abishek <abishek.genex@gmail.com>
2019-10-16 15:13:33 -0700

    Add OMP support for accumulate_weighted

--- src/arithmetic_operations/cpu/host_accumulate_weighted.hpp
+++ src/arithmetic_operations/cpu/host_accumulate_weighted.hpp
@@ -1,5 +1,82 @@
 #include <cpu/rpp_cpu_common.hpp>
 
+// With OMP
+///*
+template <typename T, typename U>
+RppStatus accumulate_weighted_host(T* srcPtr1, U* srcPtr2, RppiSize srcSize,
+                                   Rpp32f alpha,
+                                   RppiChnFormat chnFormat, Rpp32u channel)
+{
+    Rpp32f pixel;
+    Rpp32u imageDim = srcSize.height * srcSize.width;
+
+    if (chnFormat == RPPI_CHN_PLANAR)
+    {
+        for (int c = 0; c < channel; c++)
+        {
+            Rpp32u cLoc = (c * imageDim);
+            #pragma omp parallel for simd firstprivate(cLoc, pixel)
+            for (int i = 0; i < srcSize.height; i++)
+            {
+                Rpp32u iLoc = cLoc + (i * srcSize.width);
+                for (int j = 0; j < srcSize.width; j++)
+                {
+                    Rpp32u jLoc = iLoc + j;
+
+                    pixel = ((1 - alpha) * ((Rpp32f) srcPtr1[jLoc])) + (alpha * ((Rpp32f) srcPtr2[jLoc]));
+                    pixel = RPPPIXELCHECK(pixel);
+                    srcPtr1[jLoc] =(T) pixel;
+                }
+            }
+        }
+    }
+    else if (chnFormat == RPPI_CHN_PACKED)
+    {
+        Rpp32u elementsInRow = channel * srcSize.width;
+        #pragma omp parallel for simd firstprivate(pixel)
+        for (int i = 0; i < srcSize.height; i++)
+        {
+            Rpp32u iLoc = (i * elementsInRow);
+            for (int j = 0; j < srcSize.width; j++)
+            {
+                Rpp32u jLoc = iLoc + (j * channel);
+                for (int c = 0; c < channel; c++)
+                {
+                    Rpp32u cLoc = jLoc + c;
+
+                    pixel = ((1 - alpha) * ((Rpp32f) srcPtr1[cLoc])) + (alpha * ((Rpp32f) srcPtr2[cLoc]));
+                    pixel = RPPPIXELCHECK(pixel);
+                    srcPtr1[cLoc] =(T) pixel;
+                }
+            }
+        }
+    }
+
+    return RPP_SUCCESS;
+
+}
+//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Without OMP
+/*
 template <typename T, typename U>
 RppStatus accumulate_weighted_host(T* srcPtr1, U* srcPtr2, RppiSize srcSize,
                                    Rpp32f alpha,
@@ -24,3 +101,4 @@ RppStatus accumulate_weighted_host(T* srcPtr1, U* srcPtr2, RppiSize srcSize,
     return RPP_SUCCESS;
 
 }
+*/
