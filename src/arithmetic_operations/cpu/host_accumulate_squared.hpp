cac74731002e7ab7d6b20452ecf97a1409d49543
r-abishek <abishek.genex@gmail.com>
2019-10-16 15:04:47 -0700

    Add OMP support for accumulate_squared

--- src/arithmetic_operations/cpu/host_accumulate_squared.hpp
+++ src/arithmetic_operations/cpu/host_accumulate_squared.hpp
@@ -1,5 +1,82 @@
 #include <cpu/rpp_cpu_common.hpp>
 
+// With OMP
+///*
+template <typename T>
+RppStatus accumulate_squared_host(T* srcPtr, RppiSize srcSize,
+                                  RppiChnFormat chnFormat, Rpp32u channel)
+{
+    Rpp32s pixel;
+    Rpp32u imageDim = srcSize.height * srcSize.width;
+
+    if (chnFormat == RPPI_CHN_PLANAR)
+    {
+        for (int c = 0; c < channel; c++)
+        {
+            Rpp32u cLoc = (c * imageDim);
+            #pragma omp parallel for simd firstprivate(cLoc, pixel)
+            for (int i = 0; i < srcSize.height; i++)
+            {
+                Rpp32u iLoc = cLoc + (i * srcSize.width);
+                for (int j = 0; j < srcSize.width; j++)
+                {
+                    Rpp32u jLoc = iLoc + j;
+
+                    pixel = (Rpp32s) srcPtr[jLoc];
+                    pixel = pixel * pixel;
+                    pixel = RPPPIXELCHECK(pixel);
+                    srcPtr[jLoc] =(T) pixel;
+                }
+            }
+        }
+    }
+    else if (chnFormat == RPPI_CHN_PACKED)
+    {
+        Rpp32u elementsInRow = channel * srcSize.width;
+        #pragma omp parallel for simd firstprivate(pixel)
+        for (int i = 0; i < srcSize.height; i++)
+        {
+            Rpp32u iLoc = (i * elementsInRow);
+            for (int j = 0; j < srcSize.width; j++)
+            {
+                Rpp32u jLoc = iLoc + (j * channel);
+                for (int c = 0; c < channel; c++)
+                {
+                    Rpp32u cLoc = jLoc + c;
+
+                    pixel = (Rpp32s) srcPtr[cLoc];
+                    pixel = pixel * pixel;
+                    pixel = RPPPIXELCHECK(pixel);
+                    srcPtr[cLoc] =(T) pixel;
+                }
+            }
+        }
+    }
+
+    return RPP_SUCCESS;
+
+}
+//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Without OMP
+/*
 template <typename T>
 RppStatus accumulate_squared_host(T* srcPtr, RppiSize srcSize,
                                   RppiChnFormat chnFormat, Rpp32u channel)
@@ -21,3 +98,4 @@ RppStatus accumulate_squared_host(T* srcPtr, RppiSize srcSize,
     return RPP_SUCCESS;
 
 }
+*/
