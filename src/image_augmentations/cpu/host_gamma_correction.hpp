01cf994018a1f2fa17465c5dae0e81ae7032c0dc
r-abishek <abishek.genex@gmail.com>
2019-10-16 14:39:20 -0700

    Add OMP support for gamma_correction

--- src/image_augmentations/cpu/host_gamma_correction.hpp
+++ src/image_augmentations/cpu/host_gamma_correction.hpp
@@ -1,6 +1,83 @@
 #include <cpu/rpp_cpu_common.hpp>
 
 template <typename T>
+
+// With OMP
+///*
+RppStatus gamma_correction_host(T* srcPtr, RppiSize srcSize, T* dstPtr,
+                                   Rpp32f gamma,
+                                   RppiChnFormat chnFormat, Rpp32u channel)
+{
+    Rpp32f pixel;
+    Rpp32u imageDim = srcSize.height * srcSize.width;
+
+    if (chnFormat == RPPI_CHN_PLANAR)
+    {
+        for (int c = 0; c < channel; c++)
+        {
+            Rpp32u cLoc = (c * imageDim);
+            #pragma omp parallel for simd firstprivate(cLoc, pixel)
+            for (int i = 0; i < srcSize.height; i++)
+            {
+                Rpp32u iLoc = cLoc + (i * srcSize.width);
+                //#pragma omp parallel for simd firstprivate(iLoc, pixel)
+                for (int j = 0; j < srcSize.width; j++)
+                {
+                    Rpp32u jLoc = iLoc + j;
+
+                    pixel = ((Rpp32f) srcPtr[jLoc]) / 255.0;
+                    pixel = pow(pixel, gamma);
+                    pixel = pixel * 255.0;
+                    pixel = RPPPIXELCHECK(pixel);
+                    dstPtr[jLoc] = (T) pixel;
+                }
+            }
+        }
+    }
+    else if (chnFormat == RPPI_CHN_PACKED)
+    {
+        Rpp32u elementsInRow = channel * srcSize.width;
+        #pragma omp parallel for simd firstprivate(pixel)
+        for (int i = 0; i < srcSize.height; i++)
+        {
+            Rpp32u iLoc = (i * elementsInRow);
+            //#pragma omp parallel for firstprivate(iLoc, pixel)
+            for (int j = 0; j < srcSize.width; j++)
+            {
+                Rpp32u jLoc = iLoc + (j * channel);
+                for (int c = 0; c < channel; c++)
+                {
+                    Rpp32u cLoc = jLoc + c;
+
+                    pixel = ((Rpp32f) srcPtr[cLoc]) / 255.0;
+                    pixel = pow(pixel, gamma);
+                    pixel = pixel * 255.0;
+                    pixel = RPPPIXELCHECK(pixel);
+                    dstPtr[cLoc] = (T) pixel;
+                }
+            }
+        }
+    }
+
+    return RPP_SUCCESS;
+
+}
+//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Without OMP
+/*
 RppStatus gamma_correction_host(T* srcPtr, RppiSize srcSize, T* dstPtr,
                                    Rpp32f gamma,
                                    RppiChnFormat chnFormat, Rpp32u channel)
@@ -25,3 +102,4 @@ RppStatus gamma_correction_host(T* srcPtr, RppiSize srcSize, T* dstPtr,
     return RPP_SUCCESS;
 
 }
+*/
