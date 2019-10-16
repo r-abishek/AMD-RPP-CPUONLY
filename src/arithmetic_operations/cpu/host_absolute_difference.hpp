5b734057d47a8ea4eb330d56b8312d189fd5add7
r-abishek <abishek.genex@gmail.com>
2019-10-16 14:57:10 -0700

    Add OMP support for absolute_difference

--- src/arithmetic_operations/cpu/host_absolute_difference.hpp
+++ src/arithmetic_operations/cpu/host_absolute_difference.hpp
@@ -1,5 +1,77 @@
 #include <cpu/rpp_cpu_common.hpp>
 
+// With OMP
+///*
+template <typename T, typename U>
+RppStatus absolute_difference_host(T* srcPtr1, U* srcPtr2, RppiSize srcSize, T* dstPtr,
+                                   RppiChnFormat chnFormat, Rpp32u channel)
+{
+    Rpp32s pixel;
+    Rpp32u imageDim = srcSize.height * srcSize.width;
+
+    if (chnFormat == RPPI_CHN_PLANAR)
+    {
+        for (int c = 0; c < channel; c++)
+        {
+            Rpp32u cLoc = (c * imageDim);
+            #pragma omp parallel for simd firstprivate(cLoc, pixel)
+            for (int i = 0; i < srcSize.height; i++)
+            {
+                Rpp32u iLoc = cLoc + (i * srcSize.width);
+                for (int j = 0; j < srcSize.width; j++)
+                {
+                    Rpp32u jLoc = iLoc + j;
+
+                    pixel = ((Rpp32s) srcPtr1[jLoc]) - ((Rpp32s) srcPtr2[jLoc]);
+                    pixel = RPPABS(pixel);
+                    pixel = RPPPIXELCHECK(pixel);
+                    dstPtr[jLoc] =(T) pixel;
+                }
+            }
+        }
+    }
+    else if (chnFormat == RPPI_CHN_PACKED)
+    {
+        Rpp32u elementsInRow = channel * srcSize.width;
+        #pragma omp parallel for simd firstprivate(pixel)
+        for (int i = 0; i < srcSize.height; i++)
+        {
+            Rpp32u iLoc = (i * elementsInRow);
+            for (int j = 0; j < srcSize.width; j++)
+            {
+                Rpp32u jLoc = iLoc + (j * channel);
+                for (int c = 0; c < channel; c++)
+                {
+                    Rpp32u cLoc = jLoc + c;
+
+                    pixel = ((Rpp32s) srcPtr1[cLoc]) - ((Rpp32s) srcPtr2[cLoc]);
+                    pixel = RPPABS(pixel);
+                    pixel = RPPPIXELCHECK(pixel);
+                    dstPtr[cLoc] =(T) pixel;
+                }
+            }
+        }
+    }
+
+    return RPP_SUCCESS;
+
+}
+//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Without OMP
+/*
 template <typename T, typename U>
 RppStatus absolute_difference_host(T* srcPtr1, U* srcPtr2, RppiSize srcSize, T* dstPtr,
                                    RppiChnFormat chnFormat, Rpp32u channel)
@@ -26,3 +98,4 @@ RppStatus absolute_difference_host(T* srcPtr1, U* srcPtr2, RppiSize srcSize, T* 
     return RPP_SUCCESS;
 
 }
+*/
